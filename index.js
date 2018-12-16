f8d2a2d36d96694cb899b0918f0dd38bad6c61f2
kiyutink <kiyutin@gmail.com>
2018-12-16 15:46:10 +0300

    Change the CLI syntax

--- index.js
+++ index.js
@@ -7,45 +7,37 @@ const minimist = require('minimist');
 const rimraf = require('rimraf');
 const chalk = require('chalk');
 const ora = require('ora');
-const printBadArgumentsError = require('./printBadArgumentsError');
 const boilerplateRepoUrl = 'https://github.com/kiyutink/frontend-boilerplate.git';
 const args = minimist(process.argv.slice(2));
 
-if (args._.length !== 2) {
-  printBadArgumentsError();
+if (!args._[0]) {
+  console.log(chalk.red('Please specify the name of the created project'));
+  process.exit(1);
 }
+const dirName = args._[0];
+const dirPath = path.join(process.cwd(), dirName);
+if (fs.existsSync(dirPath)) {
+  console.log(
+    chalk.red(
+      'Error: the specified directory already exists. You can only initiate the boilerplate in a new directory'
+    )
+  );
+  process.exit(1);
+}
+else {
+  const spinner = ora(chalk.cyan(`Initializing a boilerplate project in ${dirPath}`));
+  spinner.start();
 
-else if (args._[0] === 'g') {
-  if (args._[1]) {
-    const dirName = args._[1];
-    const dirPath = path.join(process.cwd(), dirName);
-    if (fs.existsSync(dirPath)) {
-      console.log(
-        chalk.red(
-          'Error: the specified directory already exists. You can only initiate the boilerplate in a new directory'
-        )
-      );
-      process.exit(1);
+  Git.Clone.clone(boilerplateRepoUrl, dirPath).then(
+    () => {
+      rimraf('.git', {}, () => {
+        console.log(chalk.green('\nDone!'));
+      });
+      spinner.stop();
+    },
+    () => {
+      console.log(chalk.green('Unexpected error'));
+      spinner.stop();
     }
-    else {
-      const spinner = ora(chalk.cyan(`Initializing a boilerplate project in ${dirPath}`));
-      spinner.start();
-
-      Git.Clone.clone(boilerplateRepoUrl, dirPath).then(
-        () => {
-          rimraf('.git', {}, () => {
-            console.log(chalk.green('\nDone!'));
-          });
-          spinner.stop();
-        },
-        () => {
-          console.log(chalk.green('Unexpected error'));
-          spinner.stop();
-        }
-      );
-    }
-  }
-  else {
-    printBadArgumentsError();
-  }
+  );
 }
